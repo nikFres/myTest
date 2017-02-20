3774721dfb9e95551ab68cfaa6e89b4e54ef0d9f
Nikos Freskas <nfreskas@gmail.com>
2017-02-20 14:50:17 +0200

    remove empty spaces

--- Test/src/MyTest.java
+++ Test/src/MyTest.java
@@ -4,8 +4,7 @@ public class MyTest {
 	public static void main(String[] args) {
 		String a = "aaaa";
 		
-		String b = "aaafsdf";
-		
+		String b = "aaafsdf1";
 		
 		
 		System.out.println("ena");
